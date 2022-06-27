7fcdce87aefc1e077ba689774633d6a55d0a6de0
haricm <haricm1988@gmail.com>
2022-06-27 15:32:13 +0530

    Creating a KV store with initial vals and get fn

--- cmd/store/store.go
+++ cmd/store/store.go
@@ -17,11 +17,36 @@ func get(key string) (string, error) {
 	return v, nil
 }
 
+func put(key string, value string) {
+	store[key] = value
+	return
+}
+
+func del(key string) {
+	delete(store, key)
+	return
+}
+
 func main() {
 	location_a, err := get("user_a")
 	if(err != nil) {
 		fmt.Println(err)
-		return
+	} else {
+		fmt.Println("Location of user_a:",location_a)
 	}
-	fmt.Println("Location of user_a:",location_a)
+	del("user_a")
+	fmt.Println("user_a details removed from key-value store")
+	location_a, err = get("user_a")
+	if(err != nil) {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Location of user_a:",location_a)
+	}
+	put("user_c","Chennai, Tamilnadu")
+	location_c, err := get("user_c")
+	if(err != nil) {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Location of user_c:",location_c)
+	}
 }
